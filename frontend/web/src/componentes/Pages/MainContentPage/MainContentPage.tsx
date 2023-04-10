f31a2254b46fced0e3208b87c6ad500428272359
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-04-10 19:45:03 +0300

    changes to the layout of the main elements and adding a new main page

---
+++ frontend/web/src/componentes/Pages/MainContentPage/MainContentPage.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import './MainContentPage.scss'
+import {Link} from "react-router-dom";
+import MainImg from "../../../images/main-page-img.png";
+
+const MainContentPage = ()=>{
+return(
+    <div className={"main-page"}>
+        <h2 className={"main-page-title"}  >Онлайн-платформа для решения задач по программированию</h2>
+        <div className="main-page-content">
+            <div className="main-page-content-description">
+                <p>Обучайся, тренируйся и подготавливайся к техническим собеседованиям, решая алгоритмические задачи на 5 языках программирования с автоматической проверкой решений и разными уровнями сложности.</p>
+                <button className={"main-content-button"}> Начать сейчас! </button>
+            </div>
+            <img src={MainImg} alt={"main"}/>
+        </div>
+        <Link to={"/topics"}>Темы</Link>
+    </div>
+
+)
+
+}
+
+export default MainContentPage
