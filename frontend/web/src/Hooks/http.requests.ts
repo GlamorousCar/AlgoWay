6841f9c56d0311cd1dfc8548f8b08694b452b696
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-05-04 19:14:49 +0300

    Fixing the post registration and login error

--- frontend/web/src/Hooks/http.requests.ts
+++ frontend/web/src/Hooks/http.requests.ts
@@ -3,14 +3,12 @@ import axios from "axios";
 
 export const _baseUrl = "https://bbaunqpcv1t23s9skmhv.containers.yandexcloud.net" ;
 const $api = axios.create({
-
-    withCredentials:true,
     baseURL:_baseUrl,
-
 })
 
 $api.interceptors.request.use((config) =>{
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+     config.headers.post["Access-Control-Allow-Origin"] = '*';
     return config;
 })
 
--- frontend/web/src/services/AuthService.ts
+++ frontend/web/src/services/AuthService.ts
@@ -4,10 +4,10 @@ import {AuthResponse} from "../types/AuthResponse";
 
 export default class AuthService{
     static async login(email:string, password:string):Promise<AxiosResponse<AuthResponse>>{
-        return $api.post<AuthResponse>("/loginUser", {email, password});
+        return $api.post<AuthResponse>("/auth/login", {email, password});
     }
 
     static async registration(login:string, email:string, password:string):Promise<AxiosResponse<AuthResponse>>{
-        return $api.post<AuthResponse>("/registerUser", {login, email, password});
+        return $api.post<AuthResponse>("/auth/register", {login, email, password});
     }
 }
