87089fffad1ac5ff58c43c6a311f090512c74ccf
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-05-06 09:45:04 +0300

    Changed the display of the ThemesList.tsx in drawer component

--- frontend/web/src/componentes/ThemesList/ThemesList.tsx
+++ frontend/web/src/componentes/ThemesList/ThemesList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {IAppState} from "../../types/store";
@@ -6,6 +6,7 @@ import './ThemesList.scss'
 
 const ThemesList = () => {
     const {menu} = useSelector((state: IAppState) => state);
+    const [selectedAlgorithm, setSelectedAlgorithm] = useState<number>()
     return (
         <>
             {
@@ -14,39 +15,36 @@ const ThemesList = () => {
                         <li className={"theme-list-item"}
                             key={menuItem.theme_id}>
                             <NavLink
-                                to={`/topics`}
-                                className={"theme-list-item "}>
+                                to={`/topics/${menuItem.theme_id}`}
+                                className={"theme-list-item"}>
                                 {
                                     ({isActive}) => {
-                                       if(isActive) {
-                                           return(
-                                               <>
-                                                   <span>{menuItem.title}</span>
-                                                   <ul className={'second-layer-list'}>
-                                                       {
-                                                           menuItem.algorithms.map(algorithm => {
-                                                               return (
-                                                                   <>
-                                                                       <li className={'second-layer-list-item'}
-                                                                           key={algorithm.algorithm_id}>
-                                                                           <NavLink
-                                                                               to={`/topics/${algorithm.algorithm_id}/theory`}
-                                                                               className={"theme-list-item"}>
-                                                                               {algorithm.title}
-                                                                           </NavLink>
-                                                                       </li>
-                                                                   </>
-                                                               )
-                                                           })}
-                                                   </ul>
-                                               </>
-                                           );
-                                       }else{
-                                           return <span>{menuItem.title}</span>
-                                       }
+                                        if (isActive){
+                                            setSelectedAlgorithm(menuItem.theme_id);
+                                        }
+                                        return <span>{menuItem.title}</span>
                                     }
                                 }
                             </NavLink>
+                            <ul className={'second-layer-list'}>
+                                {
+                                    menuItem.algorithms.map(algorithm => {
+                                        if(selectedAlgorithm === menuItem.theme_id ){
+                                            return (
+                                                <>
+                                                    <li className={'second-layer-list-item'}
+                                                        key={algorithm.algorithm_id}>
+                                                        <NavLink
+                                                            to={`/topics/${menuItem.theme_id}/${algorithm.algorithm_id}/theory`}
+                                                            className={"theme-list-item"}>
+                                                            {algorithm.title}
+                                                        </NavLink>
+                                                    </li>
+                                                </>
+                                            )
+                                        }
+                                    })}
+                            </ul>
                         </li>
                     )
                 })
@@ -56,3 +54,25 @@ const ThemesList = () => {
 };
 
 export default ThemesList;
+
+// return(
+//     <>
+//         <ul className={'second-layer-list'}>
+//             {
+//                 menuItem.algorithms.map(algorithm => {
+//                     return (
+//                         <>
+//                             <li className={'second-layer-list-item'}
+//                                 key={algorithm.algorithm_id}>
+//                                 <NavLink
+//                                     to={`/topics/${algorithm.algorithm_id}/theory`}
+//                                     className={"theme-list-item"}>
+//                                     {algorithm.title}
+//                                 </NavLink>
+//                             </li>
+//                         </>
+//                     )
+//                 })}
+//         </ul>
+//     </>
+// );
