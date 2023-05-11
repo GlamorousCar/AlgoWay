3c6c9c4801f053ad753894b0b8475341e57e9601
GlamorousCar <I89267857387@yandex.ru>
2023-05-11 23:24:42 +0300

    Настроил получение списка задач для авторизированных пользователей

--- backend/internal/models/task.go
+++ backend/internal/models/task.go
@@ -3,7 +3,7 @@ package models
 type Task struct {
 	Id          int    `json:"id"`
 	AlgorithmId int    `json:"-"`
-	IsSolved    bool   `json:"is_solved"`
+	IsSolved    string `json:"is_solved"`
 	Title       string `json:"title"`
 	Content     string `json:"content"`
 }
