68d0e3c9dd890ae33c85eeb65536d102a3bdd45f
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-05-29 18:12:56 +0300

    Created link to the profile page

--- frontend/web/src/componentes/ProfileBlock/ProfileBlock.tsx
+++ frontend/web/src/componentes/ProfileBlock/ProfileBlock.tsx
@@ -1,20 +1,27 @@
 import * as React from "react";
 import './ProfileBlock.scss'
 import avatar from '../../images/Avatar.svg'
+import {Link} from "react-router-dom";
 
 const ProfileBlock = ()=>{
     return(
-        <div className={"profile-block"}>
-            <div className="content">
-                <div className="info-block">
-                    <h6 className={'name'}>D.Tore.Y</h6>
-                </div>
-                <div className="img-block">
-                    <img src={avatar} alt=""/>
-                </div>
 
-            </div>
-        </div>
+        <>
+            <Link className={"profile-link"} to={'/profile'}>
+                <div className={"profile-block"}>
+                    <div className="content">
+                        <div className="info-block">
+                            <h6 className={'name'}>D.Tore.Y</h6>
+                        </div>
+                        <div className="img-block">
+                            <img src={avatar} alt=""/>
+                        </div>
+
+                    </div>
+                </div>
+            </Link>
+        </>
+
     )
 }
 export default ProfileBlock
