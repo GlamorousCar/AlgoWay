381d68f44acb0132b818c86fe27fd02c6c145fcd
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-05-29 18:15:27 +0300

    Adding block for google authorisation

--- frontend/web/src/index.tsx
+++ frontend/web/src/index.tsx
@@ -5,14 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import * as React from "react"
 import {Provider} from "react-redux";
 import store from "./store";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-        <Provider store={store}>
-            <App/>
-        </Provider>
+    <Provider store={store}>
+                <GoogleOAuthProvider clientId="40230221374-5jlod1u5abb0j1qjhd3frucpboadikda.apps.googleusercontent.com">
+                    <App/>
+                </GoogleOAuthProvider>
+            </Provider>
 );
 
 reportWebVitals();
