c8a59320f11a8804bd07faea53bf3481b2f57893
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-06-05 23:40:19 +0300

    created a new start up page

---
+++ frontend/web/src/Pages/StartUpPage/StartUpPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ThemesList from '../../componentes/ThemesList/ThemesList'
+import './StartUpPage.scss'
+
+const StartUpPage = () => {
+  return (
+    <div className='start-up-container'>
+      <ThemesList/>
+    
+    </div>
+  )
+}
+
+export default StartUpPage
--- frontend/web/src/componentes/App/App.tsx
+++ frontend/web/src/componentes/App/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import './App.scss';
 import ContentBlock from "../ContentBlock/ContentBlock";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, useNavigate, Navigate} from "react-router-dom";
 import RegistrationPage from "../../Pages/RegistrationPage/RegistrationPage";
 import TheoryBlockPage from "../../Pages/TheoryBlockPage/TheoryBlockPage";
 import MainContentPage from "../../Pages/MainContentPage/MainContentPage";
@@ -10,21 +10,26 @@ import PracticeBlock from "../PracticeBlock/PracticeBlock";
 import useAlgoService from "../../services/UseAlgoService";
 import {useEffect} from "react";
 import {IMenu} from "../../types/types";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {menuLoaded, menuLoading, setMenuItems} from "../../store/actions";
 import LoginForm from "../LoginForm/LoginForm";
 import RegistrationForm from "../RegistrationForm/RegistrationForm";
 import ProfilePage from "../../Pages/ProfilePage/ProfilePage";
+import { IAppState } from "../../types/store";
+import { setAuthTrue } from "../../store/actions";
+import { setAuthFalse } from "../../store/actions";
+import StartUpPage from "../../Pages/StartUpPage/StartUpPage";
 
 
 
 function App() {
     const {getMenuTopics} = useAlgoService();
     const dispatch = useDispatch();
-
+    const {isAuth} = useSelector((state: IAppState) => state);
 
     useEffect(() => {
         getMenus();
+        localStorage.getItem('token') ? dispatch(setAuthTrue()) : dispatch(setAuthFalse());
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
@@ -51,9 +56,9 @@ function App() {
         <Router>
             <div className="App">
                 <Routes>
-                    <Route   element={<RegistrationPage/>}>
-                        <Route  path={"/login"} element={<LoginForm/>}/>
-                        <Route  path={"/registration"} element={<RegistrationForm/>}/>
+                    <Route   element={ <RegistrationPage/> }>
+                        <Route  path={"/login"} element={  isAuth? ( <Navigate replace to={"/"} />) :(<LoginForm/> )}/>
+                        <Route  path={"/registration"} element={ isAuth? ( <Navigate replace to={"/"} />) :(<RegistrationForm/> )}/>
                     </Route>
                     <Route element={<ContentBlock/>}>
                         <Route path={"/topics/:topicId"} element={<MainContentPage/>}/>
@@ -62,6 +67,7 @@ function App() {
                             <Route path={'practice'} element={<PracticeBlock/>}/>
                         </Route>
                         <Route path={"/profile"} element={<ProfilePage/>}/>
+                        <Route path={"/themeList"}  element={<StartUpPage/>}/>
                         <Route path={"/"} element={<MainContentPage/>}/>
                     </Route>
                 </Routes>
